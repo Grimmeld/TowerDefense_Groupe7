2db52539c93026a1c7ff9ec57bc4909d4782e2c3
Lea PARNAUDEAU <l.parnaudeau@game-sup.fr>
2025-11-28 15:28:16 +0100

    Add Camera Manager

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,16 +1,132 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Manage the movement of the camera
+    // - Move on the axe X & Z  
+    // - Rotation on axe yaw 
+
+    // Movement is manage from an empty object -> Rotation from this point
+
+    // Manage the distance of the camera from the manager
+
+    [SerializeField] private Vector3 cameraOffset;
+
+    [Header("Movement")]
+    [SerializeField] private Vector2 inputMove;
+    [SerializeField] private float speedMove;
+
+    [Header("Rotation")]
+    [SerializeField] private float speedRotation;
+    private bool turnLeft;
+    private bool turnRight;
+    private float rotationY;
+    [SerializeField] private float rangeDistance; 
+
+    private void Start()
     {
-        
+        SetOffsetCamera();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        if (inputMove != Vector2.zero)
+        {
+            float speed = Time.deltaTime * speedMove;
+
+            transform.Translate(speed * inputMove.x, 0, speed * inputMove.y);
+        }
+
+        if (turnLeft || turnRight)
+        {
+            Vector3 currentAngles = new Vector3(
+                0,
+                Mathf.LerpAngle(transform.eulerAngles.y, rotationY, Time.deltaTime * speedRotation),
+                0);
+
+
+            transform.eulerAngles = currentAngles;
+
+
+            if (turnLeft)
+            {
+                if (transform.eulerAngles.y - rotationY < rangeDistance)
+                {
+                    transform.eulerAngles = new Vector3(0, rotationY, 0);
+                    turnLeft = false;
+                }
+            }
+            else if (turnRight)
+            {
+                if (rotationY - transform.eulerAngles.y < rangeDistance)
+                {
+                    transform.eulerAngles = new Vector3(0, rotationY, 0);
+                    turnRight = false;
+                }
+            }
+        }
+
+
+
     }
+
+
+    // METHODE
+
+    private void SetOffsetCamera()
+    {
+        Camera.main.transform.position = cameraOffset;
+    }
+
+    // INPUT ACTION
+
+    public void MoveCamera(InputAction.CallbackContext context)
+    {
+        inputMove = context.ReadValue<Vector2>();
+    }
+
+    public void RotationLeft(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (!turnLeft && !turnRight)
+            {
+                turnLeft = true;
+
+                if (transform.rotation.eulerAngles.y - 90f < 0)
+                {
+                    rotationY = 270f;
+                }
+                else
+                {
+                    rotationY = transform.rotation.eulerAngles.y - 90f;
+                }
+
+            }
+        }
+    }
+
+    public void RotationRight(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (!turnRight && !turnLeft)
+            {
+                turnRight = true;
+
+                if (transform.rotation.eulerAngles.y + 90f >= 360f)
+                {
+                    rotationY = 360f;
+                }
+                else
+                {
+                    rotationY = transform.rotation.eulerAngles.y + 90f;
+                }
+
+            }
+        }
+    }
+
 }
